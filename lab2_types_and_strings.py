68f24b842319859205190fd51e85333882ff9210
Aaron Fu <aaronfu123@gmail.com>
2019-10-15 16:35:38 +0100

    fioainii

--- lab2_types_and_strings.py
+++ lab2_types_and_strings.py
@@ -14,23 +14,33 @@ class Types_and_Strings:
         print("Originally entered: "+ message)
 
         # print only first and last of the sentence
-
+        print("First char: " + message[0])
+        print("Last char: " + message[-1])
 
         # use slice notation
-
-
+        print(message[:])
+        print(message[:4])
+        print(message[2:])
+        print(message[1:2])
+        print(message[1:5:3])
         # escaping a character
-
+        print("He said \"that's fantastic\"!")
 
         # find all a's in the input word and count how many there are
-
+        x = str(message.find("a"))
+        y = str(message.count("a"))
+        print("The number of a's in the input is: " + x + "\nThere are " + y + " of a's")
+        print("Total number of characters: " + str(len(message)))
 
         # replace all occurences of the character a with the - sign
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace()
-
+        message = message.replace("a", "-")
+        print(message)
 
         # printing only characters at even index positions
+        for i in range(0, 20, 2):
+            print("index" + "[" + str(i) + "] " + message[i])
 
 
     def play_with_lists(self):
